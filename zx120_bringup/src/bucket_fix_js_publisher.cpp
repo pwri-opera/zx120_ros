bdd0a51537d35dc32965b8508bf6fee55caa9580
TaroABEJido <abet.jido.kenki@gmail.com>
2024-06-20 07:51:45 +0900

    update

--- zx120_bringup/src/bucket_fix_js_publisher.cpp
+++ zx120_bringup/src/bucket_fix_js_publisher.cpp
@@ -32,7 +32,7 @@ sensor_msgs::JointState fix_js_;
 //     tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
 // }
 
-void normalize_PI(double& theta)
+double normalize_PI(double theta)
 {
     while (abs(theta)>M_PI)
     {
@@ -45,22 +45,23 @@ void normalize_PI(double& theta)
             theta += M_PI*2.0;
         }
     }
+    return theta;
 }
 
 void swing_g2_callback (const sensor_msgs::Imu::ConstPtr& msg)
 {
     is_swing_imu_ = true;
-    swing_imu_ = msg;
+    swing_imu_ = *msg;
 }
 
-void bucket_g2_callback ()
+void bucket_g2_callback (const sensor_msgs::Imu::ConstPtr& msg)
 {
     is_bucket_imu_ = false;
     bucket_imu_ = *msg;
 }
 
 
-void Get_bucket_angle (const sensor_msgs::Imu::ConstPtr& msg)
+void Get_bucket_angle ()
 {
 
     double angle = 0.0;
@@ -85,7 +86,7 @@ void Get_bucket_angle (const sensor_msgs::Imu::ConstPtr& msg)
     quat_bucket_base_swing = quat_swing.inverse() * quat_bucket;
     tf2::Matrix3x3(quat_bucket_base_swing).getRPY(roll, pitch, yaw);
 
-    angle = pitch - fix_js_.position[BOOM] - fix_js_.position[ARM] 
+    angle = pitch - fix_js_.position[BOOM] - fix_js_.position[ARM];
     angle = normalize_PI(angle);
 
     double th_a = angle - th_os_imu_buck - th_os_arm;
@@ -94,28 +95,18 @@ void Get_bucket_angle (const sensor_msgs::Imu::ConstPtr& msg)
     double beta = acos((l3*l3 + l1*l1 - l2*l2 + l4*l4 - 2*l1*l3*cos(th_a))/(2*l4*lx));
     double th_buck =  - M_PI + alpha + beta + th_os_buck;
 
-    th_buck = normalize_PI(th_buck)
+    th_buck = normalize_PI(th_buck);
 
-    fix_js_.position[BUCKET] = 
-    fix_js_.velocity[BUCKET] = 
-
-
-    q_imu[BUCKET] = msg->orientation;
-
-    tf2::convert(q_imu[BUCKET], quat1);
-    tf2::convert(q_imu[ARM], quat0);
-
-    angle = tf2::angleShortestPath(quat0, quat1);
-    normalize_PI(angle);
-
-    imubased_js.position[BUCKET] = get_bucket_angle(angle);
-    imubased_js.velocity[BUCKET] = msg->angular_velocity.y;
+    fix_js_.position[BUCKET] = th_buck;
+    fix_js_.velocity[BUCKET] = bucket_imu_.angular_velocity.y;
 }
 
 
 
 void AC58_js_callback (const sensor_msgs::JointState::ConstPtr& msg)
 {
+    fix_js_.header.stamp = msg->header.stamp;
+
     for (int i=0; i<msg->name.size(); i++) 
     {
         if (msg->name[i] == "swing_joint")
@@ -151,7 +142,7 @@ int main(int argc, char **argv)
     is_swing_imu_   = false;
     is_bucket_imu_  = false; 
 
-    ros::Publisher imubased_js_pub = nh.advertise<sensor_msgs::JointState> ("ac58_fix_bucket_joint_publisher/joint_states", 10);
+    ros::Publisher  fix_js_pub = nh.advertise<sensor_msgs::JointState> ("ac58_fix_bucket_joint_publisher/joint_states", 10);
     ros::Subscriber swing_imu_sub = nh.subscribe ("swing/g2_imu", 5, &swing_g2_callback);
     ros::Subscriber bucket_imu_sub = nh.subscribe ("bucket/g2_imu", 5, &bucket_g2_callback);
 
@@ -160,11 +151,10 @@ int main(int argc, char **argv)
 
     while(ros::ok())
     {
-        imubased_js.header.stamp = ros::Time::now();
-        imubased_js.name = {"swing_joint", "boom_joint", "arm_joint", "bucket_joint"};
-
-        imubased_js_pub.publish(imubased_js);
-
+        fix_js_.header.stamp = ros::Time::now();
+        Get_bucket_angle();
+        fix_js_.name = {"swing_joint", "boom_joint", "arm_joint", "bucket_joint"};
+        fix_js_pub.publish(fix_js_);
         loop.sleep();
         ros::spinOnce();
     }
